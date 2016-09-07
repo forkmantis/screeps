f720b7133954bcddd5d904b6d3c7602ff6767026
Will Killian <Will Killian>
2016-09-07 15:14:55 -0500

    Added autogeneration for upgrader.

--- main.js
+++ main.js
@@ -14,9 +14,16 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+
     if(harvesters.length < 2) {
         var newName = Game.spawns['SpawnMantis'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
+    }
+    
+    if (upgraders.length < 1) {
+        var newName = Game.spawns['SpawnMantis'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
     }
 
     for(var name in Game.creeps) {
