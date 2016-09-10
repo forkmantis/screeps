e92924b35206baac0b820f45c5c6e3e21d48ba42
Will Killian <github.user@forkmantis.com>
2016-09-10 10:57:34 -0500

    Harvester now prioritizes extensions first, and will not harvest again until it has delivered to emptiness.

--- role.harvester.js
+++ role.harvester.js
@@ -1,26 +1,48 @@
+var util = require('util');
+
 var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+        
+        if(creep.memory.delivering && creep.carry.energy == 0) {
+            creep.memory.delivering = false;
+            creep.say('harvesting');
         }
-        else {
+        if(!creep.memory.delivering  && creep.carry.energy == creep.carryCapacity) {
             creep.moveTo(Game.flags.build1flag);
-            var targets = creep.room.find(FIND_STRUCTURES, {
+            creep.memory.delivering = true;
+            creep.say('delivering');
+        }
+
+        if(creep.memory.delivering) {
+            var target = util.findNearestEmptyExtension(creep);
+            if (target) {
+                creep.memory.target = target.id;
+                error = creep.transfer(Game.getObjectById(creep.memory.target), RESOURCE_ENERGY);
+                if(error == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.getObjectById(creep.memory.target));
+                }
+            }
+            else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
                                 structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                     }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
+                });
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0]);
+                    }
                 }
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
 	}
