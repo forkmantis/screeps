15e5ade7949f63a817998c9df97ae16bf3959e90
Will Killian <github.user@forkmantis.com>
2016-09-10 22:04:35 -0500

    Modify all roles to gather resources from the closest source.

--- role.builder.js
+++ role.builder.js
@@ -22,9 +22,12 @@ var roleBuilder = {
             }
 	    }
 	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            var source = creep.pos.findClosestByRange(FIND_SOURCES);
+            if (source) {
+                creep.memory.target = source.id;
+            }
+            if(creep.harvest(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.getObjectById(creep.memory.target));
             }
 	    }
 	}
--- role.upgrader.js
+++ role.upgrader.js
@@ -19,9 +19,12 @@ var roleUpgrader = {
             }
         }
         else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+            var source = creep.pos.findClosestByRange(FIND_SOURCES);
+            if (source) {
+                creep.memory.target = source.id;
+            }
+            if(creep.harvest(Game.getObjectById(creep.memory.target)) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.getObjectById(creep.memory.target));
             }
         }
 	}
