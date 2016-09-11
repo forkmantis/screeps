d0760aaeb6ce843b99d678cf3b4a19977519c687
Will Killian <github.user@forkmantis.com>
2016-09-11 09:17:31 -0500

    Added waylon531's functions for finding nearest containers to util.js.

--- util.js
+++ util.js
@@ -3,6 +3,30 @@
  */ 
 
 module.exports = {
+    findNearestContainer(creep) {
+        return creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            //Get closest container
+            filter: function(object) {
+                return object.structureType == STRUCTURE_CONTAINER;
+            }
+        });
+    },
+    findNearestFullContainer(creep) {
+        return creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            //Get closest container
+            filter: function(object) {
+                return object.structureType == STRUCTURE_CONTAINER && object.store[RESOURCE_ENERGY] > 0;
+            }
+        });
+    },
+    findNearestEmptyContainer(creep) {
+        return creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            //Get closest container
+            filter: function(object) {
+                return object.structureType == STRUCTURE_CONTAINER && _.sum(object.store) < object.storeCapacity;
+            }
+        });
+    },
     findNearestEmptyExtension(creep) {
         return creep.pos.findClosestByRange(FIND_STRUCTURES, {
             //Get closest container
