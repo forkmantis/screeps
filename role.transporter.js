6a86cca073b7abd1e091422659ca8f095f9f42bf
Will Killian <github.user@forkmantis.com>
2016-09-17 07:32:04 -0500

    transporters will not pull from containers to fill storage... need those containers full for builders and upgraders.

--- role.transporter.js
+++ role.transporter.js
@@ -21,22 +21,17 @@ var roleTransporter = {
         if(creep.memory.transporting) {
             var targetExtension = util.findNearestEmptyExtension(creep);
             var targetTurret = util.findNearestEmptyTower(creep);
-            var targetStorage = util.findNearestEmptyStorage(creep);
             if (targetExtension) {
                 creep.memory.target = targetExtension.id;
             }
             else if (targetTurret) {
                 creep.memory.target = targetTurret.id;
             }
-            else if (targetStorage) {
-                creep.memory.target = targetStorage.id;
-            }
             else {
                 var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                        return (structure.structureType == STRUCTURE_SPAWN) 
+                            && structure.energy < structure.energyCapacity;
                     }
                 });
                 if(targets.length > 0) {
